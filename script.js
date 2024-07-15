832209e505dd05b0d60c910898539a9c651d694b
snuffeen <133567875+snuffeen@users.noreply.github.com>
2024-07-15 16:01:58 +0300

    delete function done

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ class Calculator {
     }
 
     delete() {
-
+        this.currentOperand = this.currentOperand.toString().slice(0, -1)
     }
 
     appendNumber(number) {
@@ -97,5 +97,10 @@ equalsButton.addEventListener('click', button => {
 
 allClearButton.addEventListener('click', button => {
     calculator.clear()
+    calculator.updateDisplay()
+})
+
+deleteButton.addEventListener('click', button => {
+    calculator.delete()
     calculator.updateDisplay()
 })
